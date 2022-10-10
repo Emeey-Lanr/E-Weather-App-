2f65403dc062469f622057b7f53c9f8a24aaac19
Oyelowo Emmanuel <emeeylanr04@gmail.com>
2022-10-10 08:26:52 +0100

    adjusted some errors

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+///On th onload of the page, the user lat & long is accessed and used to get the waether details of their current location
 const loadgeolocation = () => {
 
     async function successCallback(position) {
@@ -10,10 +11,11 @@ const loadgeolocation = () => {
         const covertedResponse = await response.json()
         console.log(covertedResponse)
         country.innerHTML = `${covertedResponse['sys'].country}`
-        location.innerHTML = covertedResponse.name
-        temp.innerHTML = `${covertedResponse['main'].temp} <sup>o</sup>C`
-        hum.innerHTML = `${covertedResponse['main'].humidity}%`;
-        // wind.innerHTML = `${covertedResponse}`
+        document.getElementById('location').innerHTML = covertedResponse['name']
+        document.getElementById('temp').innerHTML = `${covertedResponse['main'].temp} <sup>o</sup>C`
+        document.getElementById('hum').innerHTML = `${covertedResponse['main'].humidity}%`;
+        document.getElementById('cloud').innerHTML = `${covertedResponse['main'].feels_like}<sup>o</sup>C`;
+        wind.innerHTML = `${covertedResponse.wind.speed}mph`
     }
     const errorCallback = (error) => {
         console.error(error)
@@ -21,5 +23,19 @@ const loadgeolocation = () => {
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
 
 
+}
 
+
+///Getting the user city based on the input which makes the api return weather details based on the city
+async function getCity() {
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${document.getElementById('citySearch').value}&appid=1f11e26485a68f04dbf48c27de66a81f&units=metric`
+    const response = await fetch(url)
+    const covertedResponse = await response.json()
+    country.innerHTML = `${covertedResponse['sys'].country}`
+    document.getElementById('location').innerHTML = covertedResponse['name']
+    document.getElementById('temp').innerHTML = `${covertedResponse['main'].temp} <sup>o</sup>C`
+    document.getElementById('hum').innerHTML = `${covertedResponse['main'].humidity}%`;
+    document.getElementById('cloud').innerHTML = `${covertedResponse['main'].feels_like}<sup>o</sup>C`;
+    wind.innerHTML = `${covertedResponse.wind.speed}mph`
 }
